78bbeacd483e5512caa8ad5a068fda456bc4f841
Joshua Candia <joshuacandia74@gmail.com>
2023-07-30 01:42:21 -0300

    views created

--- Client/src/views/detail/Detail.tsx
+++ Client/src/views/detail/Detail.tsx
@@ -1,11 +1,11 @@
 import style from './detail.module.scss'
 
 const Detail = () => {
-  return (
-    <div className={style.detail}>
-      <h1>Auxie</h1>
-    </div>
-  )
+    return (
+        <div className={style.detail}>
+            <h1>Detail</h1>
+        </div>
+    )
 }
 
 export default Detail
---
+++ Client/src/views/page-not-found/PageNotFound.tsx
@@ -0,0 +1,11 @@
+import style from './pageNotFound.module.scss'
+
+const PageNotFound = () => {
+    return (
+        <div className={style.pageNotFound}>
+            <h1>Page Not Found</h1>
+        </div>
+    )
+}
+
+export default PageNotFound
