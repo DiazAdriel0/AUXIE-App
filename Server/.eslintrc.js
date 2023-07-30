60c18ff4b8d4edbfe29789991b1b6daefbfde836
DiazAdriel0 <diazadriel0@gmail.com>
2023-07-30 05:20:22 -0300

    Creating backend repository

---
+++ Server/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ['standard', 'plugin:react/recommended', 'prettier'],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/prop-types': 'off',
+	},
+}
