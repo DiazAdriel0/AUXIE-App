04039532952071cca49e995a7529a58d2523f8ea
CACornelio <corneliocarlos.a@gmail.com>
2023-07-30 19:37:08 -0600

    created slice, added slice reducer to store

--- Client/src/redux/store.ts
+++ Client/src/redux/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import counterReducer from '../features/emptySlice'
 export const store = configureStore({
   reducer: {
+    counter:counterReducer,
 
-    
   },
 })
 
