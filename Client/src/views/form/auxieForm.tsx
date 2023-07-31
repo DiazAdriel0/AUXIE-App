b7cbf70f0769d7a9db3a50fa36ebcc312207b232
CACornelio <corneliocarlos.a@gmail.com>
2023-07-31 07:58:04 -0600

    inputs de clientform basico, auxieform falta poco

--- Client/src/views/form/auxieForm.tsx
+++ Client/src/views/form/auxieForm.tsx
@@ -3,8 +3,47 @@ import style from './auxieform.module.scss'
 const Form = () => {
     return (
         <div className={style.form}>
-            <h1>Form</h1>
+      <div className={style.formtitle}>Bienvenido futuro Auxie!  Completa tu registro ahora!</div>
+
+
+      <form  id='form'> 
+       
+        <div className={style.forminput}> 
+            <label>Nombre:  </label>
+            <input name='Nombre'  type='text'  className={style.textInput} placeholder='Nombre'></input>
+           
         </div>
+        <div className={style.forminput}> 
+            <label>Apellido:  </label>
+            <input name='Apellido'  type='text'  className={style.textInput}  placeholder='Apellido'></input>
+           
+          </div>
+        <div className={style.forminput}> 
+            <label>Nombre de usuario:  </label>
+            <input name='Username'  type='text'  className={style.textInput}  placeholder='Username'></input>
+         
+          </div>
+        <div className={style.forminput}> 
+            <label>Email:  </label>
+            <input name='Email'  type='email'  className={style.textInput}  placeholder='Email'></input>
+           
+          </div>
+        <div className={style.forminput}> 
+            <label>Password:  </label>
+            <input name='Password'  type='text'  className={style.textInput}  placeholder='Password'></input>
+            
+            </div>
+        <div className={style.forminput}> 
+            <label>Direccion:  </label>
+            <input name='Direccion'  type='text'  className={style.textInput}  placeholder='Direccion'></input>
+         
+             </div>
+       
+        <div className={style.submitbutton}> 
+            <input  type='submit' ></input>
+            </div>
+      </form>
+    </div>
     )
 }
 
--- Client/src/views/form/clientForm.tsx
+++ Client/src/views/form/clientForm.tsx
@@ -1,15 +1,51 @@
-import React from 'react'
+
 import style from './clientform.module.scss'
 
 
 const ClientForm = () => {
   return (
     <div className={style.form}>
-        
-        <h1>clientForm</h1>
-        
-        
+      <div className={style.formtitle}>Bienvenido a Auxie!  Completa tu registro ahora!</div>
+
+
+      <form  id='form'> 
+       
+        <div className={style.forminput}> 
+            <label>Nombre:  </label>
+            <input name='Nombre'  type='text'  className={style.textInput} placeholder='Nombre'></input>
+           
         </div>
+        <div className={style.forminput}> 
+            <label>Apellido:  </label>
+            <input name='Apellido'  type='text'  className={style.textInput}  placeholder='Apellido'></input>
+           
+          </div>
+        <div className={style.forminput}> 
+            <label>Nombre de usuario:  </label>
+            <input name='Username'  type='text'  className={style.textInput}  placeholder='Username'></input>
+         
+          </div>
+        <div className={style.forminput}> 
+            <label>Email:  </label>
+            <input name='Email'  type='email'  className={style.textInput}  placeholder='Email'></input>
+           
+          </div>
+        <div className={style.forminput}> 
+            <label>Password:  </label>
+            <input name='Password'  type='text'  className={style.textInput}  placeholder='Password'></input>
+            
+            </div>
+        <div className={style.forminput}> 
+            <label>Direccion:  </label>
+            <input name='Direccion'  type='text'  className={style.textInput}  placeholder='Direccion'></input>
+         
+             </div>
+       
+        <div className={style.submitbutton}> 
+            <input  type='submit' ></input>
+            </div>
+      </form>
+    </div>
   )
 }
 
