191773768a6f827ad01a284e12549ba51128f040
franjunoy <franjunoy@hotmail.com>
2023-07-31 13:25:30 -0300

    Ahi arregle el Models service, en provider

--- Server/Models/service.js
+++ Server/Models/service.js
@@ -1,3 +1,14 @@
 const { Schema, model } = require('mongoose')
 
+const noteSchema = new Schema({
+    name: String,
+    numberOfProviders: Number,
+    providers: Array,
+    category: String,
+    counter: Number,
+    isActive: Boolean,
+})
+
+const Service = model('Service', noteSchema)
+
 module.exports = Service
