bfe8f82cbcf95b0c8afac1b3a25b5ee3175a3209
franjunoy <franjunoy@hotmail.com>
2023-07-31 11:39:27 -0300

    Cree el Modelo Service

--- Server/Models/service.js
+++ Server/Models/service.js
@@ -1,3 +1,14 @@
 const { Schema, model } = require('mongoose')
 
+const noteSchema  = new Schema({
+        name: String, 
+        numberOfProviders: Number,
+        providers: [String],
+        category: String,
+        counter: Number,
+        isActive: Boolean,
+})
+
+const Service = model('Service', noteSchema)
+
 module.exports = Service
