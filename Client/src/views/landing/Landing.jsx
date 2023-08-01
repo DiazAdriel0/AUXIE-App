f7957301bc2104c83cd6cc04b08e88353a1e11c2
BonPato98 <bonfiglipatricio12@hotmail.com>
2023-08-01 15:17:13 -0300

    feat(client/components) added filter component

--- Client/src/views/landing/Landing.jsx
+++ Client/src/views/landing/Landing.jsx
@@ -3,11 +3,13 @@ import style from './landing.module.scss'
 //* Import components
 import Cards from '../../components/cards/cards'
 import NavLanding from '../../components/nav-landing/NavLanding'
+import Filters from '../../components/Filters/Filters'
 
 const Landing = () => {
     return (
         <div className={style.landing}>
             <NavLanding />
+            <Filters/>
             <Cards />
         </div>
     )
