a30bebcf5713b37799cd79915cea663edb01ce71
franjunoy <franjunoy@hotmail.com>
2023-08-01 10:33:10 -0300

    A ver ahi papa?

--- Server/Controllers/ServicesControllers/getAllServices.js
+++ Server/Controllers/ServicesControllers/getAllServices.js
@@ -1,14 +1,14 @@
 const { default: mongoose } = require('mongoose')
 const Service = require('../../Models/service')
 
-const getAllServices = async (serviceName, serviceCategory) => {
-    const filter = {}
+const getAllServices = async (/* serviceName, serviceCategory */) => {
+    /* const filter = {}
     if (serviceName) {
         filter.name = serviceName
     }
     if (serviceCategory) {
         filter.category = serviceCategory
-    }
+    } */
     Service.find({}).then((result) => {
         console.log(result)
         mongoose.connection.close()
--- Server/Handlers/ServicesHandlers/handlerServices.js
+++ Server/Handlers/ServicesHandlers/handlerServices.js
@@ -2,11 +2,11 @@ const getAllServices = require('../../Controllers/ServicesControllers/getAllServ
 
 const handlerServices = async (req, res) => {
     try {
-        const { name, category } = req.query
-        if (!name && !category) {
-            const allServices = await getAllServices()
-            res.status(200).json({ allServices })
-        }
+        /* const { name, category } = req.query
+        if (!name && !category) { */
+        const allServices = await getAllServices()
+        res.status(200).json({ allServices })
+        /* }
         if (name && !category) {
             const servicesByName = await getAllServices({
                 name: { $regex: name, $options: 'i' },
@@ -30,7 +30,7 @@ const handlerServices = async (req, res) => {
             throw new Error(
                 'Solo se puede proporcionar un parámetro de búsqueda'
             )
-        }
+        } */
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
