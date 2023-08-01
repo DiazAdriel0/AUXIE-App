eb89100e77a29ca1622d81b7aa265dfd8a50b829
franjunoy <franjunoy@hotmail.com>
2023-08-01 02:17:28 -0300

    Cree el handler y controllers de los Servicios

---
+++ Server/Controllers/ServicesControllers/getAllServices.js
@@ -0,0 +1,18 @@
+const { default: mongoose } = require('mongoose')
+const Service = require('../../Models/service')
+
+const getAllServices = async (serviceName, serviceCategory) => {
+    const filter = {}
+    if (serviceName) {
+        filter.name = serviceName
+    }
+    if (serviceCategory) {
+        filter.category = serviceCategory
+    }
+    Service.find({}).then((result) => {
+        console.log(result)
+        mongoose.connection.close()
+    })
+}
+
+module.exports = getAllServices
