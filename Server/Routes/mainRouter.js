6ee1d3d6a360c1ba16efe01468340d6660fa651b
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-01 11:46:35 -0300

    Connection server with routes

--- Server/Routes/mainRouter.js
+++ Server/Routes/mainRouter.js
@@ -1,11 +1,11 @@
 const { Router } = require('express')
-const usersRouter = require('./Routers/usersRouter')
+const consumersRouter = require('./Routers/consumersRouter')
 const servicesRouter = require('./Routers/servicesRouter')
 const providersRouter = require('./Routers/providersRouter')
 
 const mainRouter = Router()
 
-mainRouter.use('/users', usersRouter)
+mainRouter.use('/users', consumersRouter)
 mainRouter.use('/services', servicesRouter)
 mainRouter.use('/providers', providersRouter)
 
