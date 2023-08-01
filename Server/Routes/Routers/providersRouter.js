5586a28c344608273391859bebaf92e9a640d0da
milagros <miliguzman0@gmail.com>
2023-08-01 20:18:55 -0300

    added routes getProv,postConsumer - login-delete

--- Server/Routes/Routers/providersRouter.js
+++ Server/Routes/Routers/providersRouter.js
@@ -1,9 +1,14 @@
 const { Router } = require('express')
 const postProvider = require('./../../Handlers/ProvidersHandlers/postPorovider')
+const getProviders = require('../../Handlers/ProvidersHandlers/getProviders')
+const deleteProviderById = require('../../Handlers/ProvidersHandlers/deleteProviderById')
+const loginProvider = require('../../Handlers/ProvidersHandlers/loginProvider')
 
 const providersRouter = Router()
 
-// providersRouter.get('/', getProviders)
+providersRouter.get('/', getProviders)
 providersRouter.post('/', postProvider)
+providersRouter.get('/login', loginProvider)
+providersRouter.delete('/delete/:id', deleteProviderById )
 
 module.exports = providersRouter
