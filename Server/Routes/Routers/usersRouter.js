32550ed332fdd99bf0e4db13a7bcb6d7246d5b0a
DiazAdriel0 <diazadriel0@gmail.com>
2023-07-31 21:34:05 -0300

    delete usersRouter file

--- Server/Routes/Routers/usersRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-
-const usersRouter = Router()
-
-// usersRouter.get('/providers', getProviders)
-// usersRouter.get('/consumer', getConsumers)
-//comentario
-module.exports = usersRouter
