5c5405b78c43aff219f579d9e58e26121d737e1f
CACornelio <corneliocarlos.a@gmail.com>
2023-08-01 13:16:43 -0600

    password length and email format  validation

--- Client/src/views/Login/clientLogin.jsx
+++ Client/src/views/Login/clientLogin.jsx
@@ -6,15 +6,23 @@ const ClientLogin = () => {
         password: '',
     })
 
+    const [errors, setErrors] = useState({
+        email: '',
+        password: '',
+    })
     const handleChange = (event) => {
         console.log(event)
         setInput({
             ...input,
             [event.target.name]: event.target.value,
         })
-        // validate({
-        //   ...input,
-        //   [event.target.name]: event.target.value}, event.target.name)
+        validate(
+            {
+                ...input,
+                [event.target.name]: event.target.value,
+            },
+            event.target.name
+        )
     }
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -24,6 +32,62 @@ const ClientLogin = () => {
         form.reset()
         //navigate home / search auxies ///
     }
+
+    ///validations ///
+    const validate = (input, name) => {
+        if (name === 'email') {
+            if (input.email !== '') {
+                // Use a regular expression to check if the input value is a valid email
+                const emailPattern = new RegExp(
+                    /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/ //eslint-disable-line
+                )
+                if (emailPattern.test(input.email)) {
+                    setErrors({ ...errors, email: '' })
+                } else {
+                    setErrors({ ...errors, email: 'Invalid email format' })
+                }
+            } else {
+                setErrors({ ...errors, email: 'Email is required' })
+            }
+        }
+        if (name === 'password') {
+            //password length//
+            if (input.password.length.trim() < 8 || input.password.length.trim() > 12) {
+                setErrors({
+                    ...errors,
+                    password:
+                        'password must be between 8 and 12 characters long',
+                })
+            } else if (
+                input.password.length >= 8 &&
+                input.password.length <= 12
+            ) {
+                setErrors({ ...errors, password: '' })
+            }
+            //password length//
+        }
+    }
+
+    ///validations ///
+
+    //////para desabilitar el boton si no esta lleno el formulario=>
+    const buttonDisabled = () => {
+        // Check if the "types" field is empty
+        // if (input.types.length === 0) {
+        //   return true;
+        // }
+
+        // Check if any error message is not empty for other fields
+        for (let error in errors) {
+            if (errors[error] !== '') {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    //////
 
     return (
         <div className={style.login}>
@@ -41,6 +105,7 @@ const ClientLogin = () => {
                             placeholder="Correo electronico"
                             onChange={handleChange}
                         ></input>
+                        <p>{errors.email}</p>
                     </div>
                     <div className={style.logininput}>
                         <label>Password: </label>
@@ -52,6 +117,7 @@ const ClientLogin = () => {
                             onChange={handleChange}
                         ></input>
                     </div>
+                    <p>{errors.password}</p>
                     <div className={style.submitbutton}>
                         <input type="submit"></input>
                     </div>
