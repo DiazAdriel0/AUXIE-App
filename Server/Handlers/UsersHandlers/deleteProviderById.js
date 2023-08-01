38c0bd95cd46220f038e42f3d65b2c2b37fae55b
milagros <miliguzman0@gmail.com>
2023-07-31 21:30:17 -0300

    added controllers for delete and handlers

---
+++ Server/Handlers/UsersHandlers/deleteProviderById.js
@@ -0,0 +1,17 @@
+const deleteProvider = require('../../Controllers/UserControllers/deleteProvider')
+
+const deleteProviderById = async (req, res)=>{
+    const {id} = req.params
+
+    try {
+        const deleted = await deleteProvider(id)
+
+        if (!deleted) return res.status(400).json({error: 'Id invalido'})
+
+        res.status(200)
+
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }   
+}
+module.exports = deleteProviderById
