a1f07783bfb96d9c3ffd41730a766e2ae9bace49
Joshua Candia <joshuacandia74@gmail.com>
2023-08-01 10:38:01 -0300

    views Created

---
+++ Client/src/views/howItWorks/HowItWorks.jsx
@@ -0,0 +1,11 @@
+import style from './howItWorks.module.scss'
+
+const HowItWorks = () => {
+    return (
+        <div className={style.howItWorks}>
+            <h3>¿Cómo funciona Auxie?</h3>
+        </div>
+    )
+}
+
+export default HowItWorks
