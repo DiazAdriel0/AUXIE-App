827a256c81eb0321ebc3630a37c8eeba3450bb63
franjunoy <franjunoy@hotmail.com>
2023-08-01 22:59:09 -0300

    Modelo Service arreglado

--- Server/Models/service.js
+++ Server/Models/service.js
@@ -1,11 +1,49 @@
 const { Schema, model } = require('mongoose')
 
 const serviceSchema = new Schema({
-    name: String,
-    numberOfProviders: Number,
-    providers: Array,
-    category: String,
-    isActive: Boolean,
+    counter: {
+        type: Number,
+        default: 0,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    providers: {
+        type: [String],
+    },
+    numberOfProviders: {
+        type: Number,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+})
+serviceSchema.pre('save', function (next) {
+    this.numberOfProviders = this.providers.length
+    if (this.isNew) {
+        Service.findOne(
+            {},
+            {},
+            { sort: { counter: -1 } },
+            (err, lastService) => {
+                if (err) {
+                    return next(err)
+                }
+                if (lastService) {
+                    this.counter = lastService.counter + 1
+                }
+                next()
+            }
+        )
+    } else {
+        next()
+    }
 })
 
 const Service = model('Service', serviceSchema)
