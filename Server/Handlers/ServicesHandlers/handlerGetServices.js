6a9c9b3c5f1abf9066fda48b46e953d4a4952327
franjunoy <franjunoy@hotmail.com>
2023-08-02 10:47:56 -0300

    preuba 2

--- Server/Handlers/ServicesHandlers/handlerGetServices.js
+++ Server/Handlers/ServicesHandlers/handlerGetServices.js
@@ -9,10 +9,32 @@ const handlerServices = async (req, res) => {
             throw new Error('No se encontraron los servicios')
         }
         res.status(200).json(allServices)
-        
 
-
-        
+        /* }
+        if (name && !category) {
+            const servicesByName = await getAllServices({
+                name: { $regex: name, $options: 'i' },
+            })
+            if (!servicesByName || servicesByName.length === 0) {
+                throw new Error('No se encontraron servicios con ese nombre')
+            } else {
+                res.status(200).json(servicesByName)
+            }
+        }
+        if (!name && category) {
+            const servicesByCategory = await getAllServices({
+                category: { $regex: category, $options: 'i' },
+            })
+            if (!servicesByCategory || servicesByCategory.length === 0) {
+                throw new Error('No se encontraron categorias con ese nombre')
+            } else {
+                res.status(200).json(servicesByCategory)
+            }
+        } else {
+            throw new Error(
+                'Solo se puede proporcionar un parámetro de búsqueda'
+            )
+        } */
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
