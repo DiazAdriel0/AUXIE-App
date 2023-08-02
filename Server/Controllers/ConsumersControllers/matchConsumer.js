9d80160477838cdb4805c5f2b156293bc1273986
milagros <miliguzman0@gmail.com>
2023-08-02 17:59:45 -0300

    const

--- Server/Controllers/ConsumersControllers/matchConsumer.js
+++ Server/Controllers/ConsumersControllers/matchConsumer.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt')
 
 const matchConsumer = async (email, password) => {
     try {
-        let consumer = await Consumer.findOne({ email })
+        const consumer = await Consumer.findOne({ email })
         if (consumer) {
             console.log(consumer)
             const passwordMatch = await bcrypt.compare(
@@ -24,7 +24,7 @@ const matchConsumer = async (email, password) => {
                 registerDate: consumer.registerDate,
                 id:consumer._id
             }
-            
+
             return passwordMatch ? consumerWithout : new Error('wrongPassword')
         } else {
             throw new Error('inexistente')
