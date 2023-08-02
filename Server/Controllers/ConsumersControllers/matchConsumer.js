75292f70cc2b7e98e518c83cc17024b0e359d75d
milagros <miliguzman0@gmail.com>
2023-08-02 17:58:44 -0300

    fixed response from back to front

--- Server/Controllers/ConsumersControllers/matchConsumer.js
+++ Server/Controllers/ConsumersControllers/matchConsumer.js
@@ -3,18 +3,36 @@ const bcrypt = require('bcrypt')
 
 const matchConsumer = async (email, password) => {
     try {
-        const consumer = await Consumer.findOne({ email })
+        let consumer = await Consumer.findOne({ email })
         if (consumer) {
+            console.log(consumer)
             const passwordMatch = await bcrypt.compare(
                 password,
                 consumer.password
             )
-
-            return passwordMatch ? consumer : new Error('wrongPassword')
+            const consumerWithout = {
+                isActive: true,
+                isAdmin: false,
+                firstName: consumer.firstName,
+                lastName: consumer.lastName,
+                age: consumer.age,
+                email: consumer.email,
+                username: consumer.username,
+                ratings: consumer.ratings,
+                favoritesProviders:consumer.favoritesProviders,
+                requiredServices: consumer.requiredServices,
+                registerDate: consumer.registerDate,
+                id:consumer._id
+            }
+            
+            return passwordMatch ? consumerWithout : new Error('wrongPassword')
         } else {
             throw new Error('inexistente')
         }
     } catch (error) {
+        
+        console.log(error.message)
+
         return error
     }
 }
--- Server/Controllers/ProvidersControllers/matchProvider.js
+++ Server/Controllers/ProvidersControllers/matchProvider.js
@@ -9,7 +9,22 @@ const matchProvider = async (email, password) => {
                 password,
                 provider.password
             )
-            return passwordMatch ? provider : new Error('wrongPassword')
+            const providerWithout = {
+                id: provider._id,
+                isActive: provider.isActive,
+                firstName: provider.firstName,
+                lastName: provider.lastName,
+                age: provider.age,
+                email: provider.email,
+                username: provider.username,
+                registerDate: provider.registerDate,
+                services: provider.services,
+                pendingServices: provider.pendingServices,
+                completedWorks: provider.completedWorks,
+                ratings: provider.ratings,
+                reviews: provider.reviews
+            }
+            return passwordMatch ? providerWithout : new Error('wrongPassword')
         } else {
             throw new Error('inexistente')
         }
