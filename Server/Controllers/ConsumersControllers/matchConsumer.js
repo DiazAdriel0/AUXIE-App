ac60f4b8f3008b3b97eebffa8e24111883bfbaf6
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-02 13:46:58 -0300

    name changed from loginC to matchConsumer

---
+++ Server/Controllers/ConsumersControllers/matchConsumer.js
@@ -0,0 +1,22 @@
+const Consumer = require('../../Models/consumer')
+const bcrypt = require('bcrypt')
+
+const matchConsumer = async (email, password) => {
+    try {
+        const consumer = await Consumer.findOne({ email })
+        if (consumer) {
+            const passwordMatch = await bcrypt.compare(
+                password,
+                consumer.password
+            )
+            console.log(consumer)
+            return passwordMatch ? consumer : new Error('wrongPassword')
+        } else {
+            throw new Error('inexistente')
+        }
+    } catch (error) {
+        return error.message
+    }
+}
+
+module.exports = matchConsumer
