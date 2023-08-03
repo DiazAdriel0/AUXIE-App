b657a19f54616769e6ead0cc313fad316059a7c4
franjunoy <franjunoy@hotmail.com>
2023-08-03 10:47:50 -0300

    Borre comentarios

--- Server/Controllers/ServicesControllers/getAllServices.js
+++ Server/Controllers/ServicesControllers/getAllServices.js
@@ -2,13 +2,6 @@ const Service = require('../../Models/service')
 
 const getAllServices = async (/* serviceName, serviceCategory */) => {
     try {
-        /* const filter = {}
-    if (serviceName) {
-        filter.name = serviceName
-    }
-    if (serviceCategory) {
-        filter.category = serviceCategory
-    } */
         const services = await Service.find({})
         if (services.length === 0) {
             throw new Error('No hay servicios')
