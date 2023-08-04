3477198e4377420c77379483b3a6a391b8f5d621
franjunoy <franjunoy@hotmail.com>
2023-08-04 01:42:11 -0300

    confirmame este ya que estas

---
+++ Server/Handlers/ClaimsHandlers/handlerGetClaim.js
@@ -0,0 +1,15 @@
+const getClaim = require('../../Controllers/ClaimsControllers/getClaim')
+
+const handlerServices = async (req, res) => {
+    try {
+        const claim = await getClaim()
+        if (claim.message === 'No hay reclamos') {
+            throw new Error('No se encontraron reclamos')
+        }
+        res.status(200).json(claim)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+module.exports = handlerServices
