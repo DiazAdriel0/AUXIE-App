b337620ebe8d09bb628c1410e314571b74842529
CACornelio <corneliocarlos.a@gmail.com>
2023-08-03 18:52:33 -0600

    newhome

--- Client/src/views/home/home-consumer/HomeConsumer.jsx
+++ Client/src/views/home/home-consumer/HomeConsumer.jsx
@@ -1,25 +1,15 @@
-import style from './home.module.scss'
-import Cards from '../../../components/cards/cards'
+import style from './homeConsumer.module.scss'
+import Cards from '../../../components/Cards/Cards'
 import Filters from '../../../components/Filters/Filters'
-import NavLanding from '../../../components/nav-landing/NavLanding'
 
-const Home = () => {
+const HomeConsumer = () => {
     return (
-        <>
-            <NavLanding />
-            <div className={style.title}>
-                    <h1>Home</h1>
-                </div>
-            <div className={style.contHome}>
-                <div className={style.filters}>
-                    <Filters />
-                </div>
-                <div className={style.cards}>
-                    <Cards />
-                </div>
-            </div>
-        </>
+        <div className={style.home}>
+            <h1>Home</h1>
+            <Filters />
+            <Cards />
+        </div>
     )
 }
 
-export default Home
+export default HomeConsumer
