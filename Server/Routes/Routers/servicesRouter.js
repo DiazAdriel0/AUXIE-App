360c9de2eb1d4491ea90f9407a37dae709e7b495
franjunoy <franjunoy@hotmail.com>
2023-08-03 21:32:53 -0300

    Cree el delete Service

--- Server/Routes/Routers/servicesRouter.js
+++ Server/Routes/Routers/servicesRouter.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const handlerGetServices = require('../../Handlers/ServicesHandlers/handlerGetServices')
 const handlerPostService = require('../../Handlers/ServicesHandlers/handlerPostService')
 //const handlerPutServices = require('../../Handlers/ServicesHandlers/')
+const handlerDeleteService = require('../../Handlers/ServicesHandlers/handlerDeleteService')
 
 const servicesRouter = Router()
 
@@ -10,5 +11,7 @@ servicesRouter.get('/', handlerGetServices)
 servicesRouter.post('/', handlerPostService)
 
 //servicesRouter.put('/', handlerPutService)
+
+servicesRouter.delete('/:id', handlerDeleteService)
 
 module.exports = servicesRouter
