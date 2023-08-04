37a49253008e25401a6f439f66cbc68f2cd1ac33
BonPato98 <bonfiglipatricio12@hotmail.com>
2023-08-03 22:06:23 -0300

    se me rompió todo con el cambio de carpetas

--- Client/src/views/home/home-consumer/HomeConsumer.jsx
+++ Client/src/views/home/home-consumer/HomeConsumer.jsx
@@ -1,21 +1,14 @@
 import style from './homeConsumer.module.scss'
 import Cards from '../../../components/Cards/Cards'
 import Filters from '../../../components/Filters/Filters'
-import NavLanding from '../../../components/nav-landing/NavLanding'
 
 const HomeConsumer = () => {
     return (
-        <>
-            <NavLanding />
-            <div className={style.contHome}>
-                <div className={style.filters}>
-                    <Filters />
-                </div>
-                <div className={style.cards}>
-                    <Cards />
-                </div>
-            </div>
-        </>
+        <div className={style.home}>
+            <h1>Home</h1>
+            <Filters />
+            <Cards />
+        </div>
     )
 }
 
