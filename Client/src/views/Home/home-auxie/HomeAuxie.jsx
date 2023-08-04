b535e4168537ffed4e7d6c60c4bb8f90232f40e1
Joshua Candia <joshuacandia74@gmail.com>
2023-08-04 00:52:46 -0300

    change h1

--- Client/src/views/Home/home-auxie/HomeAuxie.jsx
+++ Client/src/views/Home/home-auxie/HomeAuxie.jsx
@@ -3,7 +3,7 @@ import style from './homeAuxie.module.scss'
 const HomeAuxie = () => {
     return (
         <div className={style.home}>
-            <h1>Home</h1>
+            <h1>In progress...</h1>
         </div>
     )
 }
