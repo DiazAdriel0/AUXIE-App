44b84865dbd303cd335a581d91f22f501f3dfb69
franjunoy <franjunoy@hotmail.com>
2023-08-03 21:33:40 -0300

    Cree el modelo claims

--- Server/Models/claims.js
+++ Server/Models/claims.js
@@ -1,7 +1,30 @@
 const { Schema, model } = require('mongoose')
 
 const consumerSchema = new Schema({
-
+    consumerUsername: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    providerUsername: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    reason: {
+        type: String,
+        required: true,
+    },
+    pending: {
+        type: Boolean,
+        default: true,
+    },
 })
 
 const Claims = model('Claims', consumerSchema)
