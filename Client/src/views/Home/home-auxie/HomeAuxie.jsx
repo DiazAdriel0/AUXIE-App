b8702b98c1ac6c3ea97f82a8b848aee2eb6796a1
Joshua Candia <joshuacandia74@gmail.com>
2023-08-06 18:41:35 -0300

    small changes

--- Client/src/views/Home/home-auxie/HomeAuxie.jsx
+++ Client/src/views/Home/home-auxie/HomeAuxie.jsx
@@ -1,11 +1,15 @@
 import style from './homeAuxie.module.scss'
 
+import { useSelector } from 'react-redux'
 //Import components
+import CardServices from '../../../components/card-services/CardServices'
 import AsideAuxie from '../../../components/home-auxie-components/aside-auxie/AsideAuxie'
 const HomeAuxie = () => {
+    const loggedUser = useSelector((state) => state.loggedUser)
+    console.log(loggedUser)
+    const { services } = loggedUser
     return (
         <div className={style.home}>
-
             {/* Header */}
             <header className={style.header}>Nav Main</header>
             {/* Aside */}
@@ -14,6 +18,22 @@ const HomeAuxie = () => {
             <main className={style.main}>
                 <div className={style.services}>
                     <h3>Servicios</h3>
+                    <div className={style.userServices}>
+                        {services ? (
+                            services.map((service) => (
+                                <CardServices
+                                    key={service.name}
+                                    name={service.name}
+                                    services={service}
+                                />
+                            ))
+                        ) : (
+                            <p>No ofrece servicios</p>
+                        )}
+                    </div>
+                    <div className={style.inbox}>
+                        <p>Puntuacion de los ultimos servicios</p>
+                    </div>
                 </div>
                 <div className={style.payments}>
                     <h3>Pagos</h3>
@@ -21,7 +41,6 @@ const HomeAuxie = () => {
             </main>
             {/* Footer */}
             <footer className={style.footer}>Pie de página</footer>
-
         </div>
     )
 }
