efa02ef056955733517bbc21a8e51e88994650e4
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-06 23:41:12 -0300

    name in services

--- Server/Controllers/ProvidersControllers/modifyProviderServices.js
+++ Server/Controllers/ProvidersControllers/modifyProviderServices.js
@@ -19,12 +19,16 @@ const modifyProviderServices = async (services, providerId) => {
                     updatedServices.push({
                         id: serviceFound._id.toString(),
                         price: service.price,
+                        //provisorio
+                        name: serviceFound.name,
                     })
                 }
             } else {
                 updatedServices.push({
                     id: serviceFound._id.toString(),
                     price: service.price,
+                    //provisorio
+                    name: serviceFound.name,
                 })
             }
             //Si el provider no estaba suscrito al servicio se agrega al array providers de la coleccion services
