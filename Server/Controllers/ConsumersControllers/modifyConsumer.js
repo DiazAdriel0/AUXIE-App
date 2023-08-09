1315625dfadb3e9f027c299cca8add6bddcaa0ae
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-09 01:48:38 -0300

    Cloudinary implemented

--- Server/Controllers/ConsumersControllers/modifyConsumer.js
+++ Server/Controllers/ConsumersControllers/modifyConsumer.js
@@ -1,35 +1,49 @@
 const Consumer = require('../../Models/consumer')
+const { uploadProfileImageToConsumer } = require('../../Utils/cloudinary')
 
-const modifyConsumer = async (req)=>{
-    const {firstName, lastName, address, image, username, id} = req.body
+const modifyConsumer = async (req) => {
+    const { firstName, lastName, address, username, id } = req.body
     try {
-        const recibedProperties ={
+        const recibedProperties = {
             id,
             firstName,
             lastName,
             address,
             username,
-            usernameLower:username?.toLowerCase(),
-            image,
+            usernameLower: username?.toLowerCase(),
         }
-    
+
+        if (req.files?.image) {
+            const result = await uploadProfileImageToConsumer(
+                req.files.image.tempFilePath
+            )
+            recibedProperties.image = {
+                public_id: result.public_id,
+                secure_url: result.secure_url,
+            }
+        }
+
         const filledProperties = Object.entries(recibedProperties)
             // eslint-disable-next-line no-unused-vars
             .filter(([_, value]) => {
                 return (
                     (typeof value === 'string' && value.trim() !== '') ||
-                    (Array.isArray(value) && value.length > 0)
+                    (Array.isArray(value) && value.length > 0) ||
+                    typeof value === 'object'
                 )
             })
-    
-        const filledObject = Object.fromEntries(filledProperties)
-        const consumer = await Consumer.updateOne({_id:id}, filledObject)
+            .map(([key, value]) => [key, value])
 
-        if(consumer.modifiedCount===0) throw new Error('No se pudo actualizar')
+        const filledObject = Object.fromEntries(filledProperties)
+
+        const consumer = await Consumer.updateOne({ _id: id }, filledObject)
+
+        if (consumer.modifiedCount === 0)
+            throw new Error('No se pudo actualizar')
 
         return consumer
-        
     } catch (error) {
+        console.error(error)
         return false
     }
 }
--- Server/Handlers/ProvidersHandlers/updateProvider.js
+++ Server/Handlers/ProvidersHandlers/updateProvider.js
@@ -1,7 +1,8 @@
 const findAndUpdateProvider = require('./../../Controllers/ProvidersControllers/findAndUpdateProvider')
+const { uploadProfileImageToProvider } = require('./../../Utils/cloudinary')
 
 const updateProvider = async (req, res) => {
-    const { id, firstName, lastName, address, image, username, services, bio } =
+    const { id, firstName, lastName, address, username, services, bio } =
         req.body
 
     const recibedProperties = {
@@ -9,11 +10,20 @@ const updateProvider = async (req, res) => {
         firstName,
         lastName,
         address,
-        image,
         username,
         usernameLower: username?.toLowerCase(),
         services,
         bio,
+    }
+
+    if (req.files?.image) {
+        const result = await uploadProfileImageToProvider(
+            req.files.image.tempFilePath
+        )
+        recibedProperties.image = {
+            public_id: result.public_id,
+            secure_url: result.secure_url,
+        }
     }
 
     const filledProperties = Object.entries(recibedProperties)
@@ -21,7 +31,8 @@ const updateProvider = async (req, res) => {
         .filter(([_, value]) => {
             return (
                 (typeof value === 'string' && value.trim() !== '') ||
-                (Array.isArray(value) && value.length > 0)
+                (Array.isArray(value) && value.length > 0) ||
+                typeof value === 'object'
             )
         })
         .map(([key, value]) => [key, value])
