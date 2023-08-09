ce4a14ecc3b6b8a79ed52390a0fe367bc0d0433d
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-09 02:51:56 -0300

    Services put updated

--- Server/Handlers/ServicesHandlers/handlerPutService.js
+++ Server/Handlers/ServicesHandlers/handlerPutService.js
@@ -1,12 +1,22 @@
 const putService = require('../../Controllers/ServicesControllers/putService')
+const { uploadServiceImage } = require('./../../Utils/cloudinary')
 
 const handlerPutService = async (req, res) => {
     try {
-        const { category, name, image } = req.body
+        const { category, name } = req.body
         const { id } = req.params
+        let image
 
-        if (!name && !category && !image) {
+        if (!name && !category) {
             throw new Error('Faltan datos')
+        } else if (req.files?.image) {
+            const result = await uploadServiceImage(
+                req.files.image.tempFilePath
+            )
+            image = {
+                public_id: result.public_id,
+                secure_url: result.secure_url,
+            }
         } else {
             const updatedService = await putService(id, category, name, image)
 
