dd4e6ed0c3c8deddba31589abc4be649a7fc7e18
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-10 11:55:03 -0300

    deploy

--- Client/src/config/firebase-config.js
+++ Client/src/config/firebase-config.js
@@ -4,24 +4,9 @@ import { GoogleAuthProvider, getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-// const firebaseConfig = import.meta.env.NEXT_PUBLIC_FIREBASE_CONFIG
+const firebaseConfig = import.meta.env.NEXT_PUBLIC_FIREBASE_CONFIG
 // Your web app's Firebase configuration
-const apiKey = import.meta.env.VITE_API_KEY
-const authDomain = import.meta.env.VITE_AUTH_DOMAIN
-const projectId = import.meta.env.VITE_PROJECT_ID
-const storageBucket = import.meta.env.VITE_STORAGE_BUCKET
-const messagingSenderId = import.meta.env.VITE_MESSAGING_SENDER_ID
-const appId = import.meta.env.VITE_APP_ID
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: apiKey,
-    authDomain: authDomain,
-    projectId: projectId,
-    storageBucket: storageBucket,
-    messagingSenderId: messagingSenderId,
-    appId: appId,
-}
-
+console.log(firebaseConfig)
 // Initialize Firebase
 const app = initializeApp(JSON.parse(firebaseConfig))
 export const auth = getAuth(app)
