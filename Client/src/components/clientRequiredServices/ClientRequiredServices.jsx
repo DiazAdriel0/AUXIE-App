9789eb82cc3e694495ce943adf2b015cfc904ae9
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-10 11:30:08 -0300

    fix

--- Client/src/components/clientRequiredServices/ClientRequiredServices.jsx
+++ Client/src/components/clientRequiredServices/ClientRequiredServices.jsx
@@ -1,18 +1,25 @@
-import { useSelector } from "react-redux"
-import style from './clientRequiredServices'
-import ClientRequiredService from "../clientRequiredService/clientRequiredService"
+import { useSelector } from 'react-redux'
+import style from './clientRequiredServices.module.scss'
+import ClientRequiredService from '../clientRequiredService/clientRequiredService'
 
-const clientRequiredServices = () => {
+const ClientRequiredServices = () => {
     const client = useSelector((state) => state.loggedUser)
-  return (
-    <>
-    <div className={style.clientServices}>
-    {client.requiredServices.length > 0 && client.requiredServices.map(service => {
-        <ClientRequiredService firstName={service.firstName} lastName={service.lastName} name={service.name} status={service.status}/>
-    })}
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className={style.clientServices}>
+                {client.requiredServices.length > 0 &&
+                    client.requiredServices.map((service, index) => (
+                        <ClientRequiredService
+                            key={index}
+                            firstName={service.firstName}
+                            lastName={service.lastName}
+                            name={service.name}
+                            status={service.status}
+                        />
+                    ))}
+            </div>
+        </>
+    )
 }
 
-export default clientRequiredServices
+export default ClientRequiredServices
