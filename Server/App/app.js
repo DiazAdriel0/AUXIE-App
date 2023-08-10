005be2b0339e0ae618841865ab22942a92435444
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-10 12:37:18 -0300

    all domains

--- Server/App/app.js
+++ Server/App/app.js
@@ -19,10 +19,7 @@ server.use(cookieParser())
 server.use(morgan('dev'))
 
 server.use((req, res, next) => {
-    res.header(
-        'Access-Control-Allow-Origin',
-        process.env ? 'http://localhost:5173' : 'https://auxie-app.vercel.app'
-    )
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Credentials', 'true')
     res.header(
         'Access-Control-Allow-Headers',
