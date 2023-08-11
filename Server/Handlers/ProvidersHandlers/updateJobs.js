f7bfc23c25e5bb2a917a81dd9f4a7acd8035f7dc
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-11 02:23:54 -0300

    mail added

--- Server/Handlers/ProvidersHandlers/updateJobs.js
+++ Server/Handlers/ProvidersHandlers/updateJobs.js
@@ -8,6 +8,31 @@ const updateJobs = async (req, res) => {
         if (addService.message)
             throw new Error('No se pudo agregar el servicio')
 
+        let pronoun
+
+        // prettier-ignore
+        switch (gender) {
+        case 'Masculino':
+            pronoun = 'o'
+            break
+        case 'Femenino':
+            pronoun = 'a'
+            break
+        case 'Otro':
+            pronoun = 'e'
+            break
+        default:
+            pronoun = 'x'
+            break
+        }
+
+        await transporter.sendMail({
+            from: `Team Auxie ${process.env.EMAIL}`,
+            to: email,
+            subject: `Bienvenid${pronoun} ${firstName}`,
+            text: `Bienvenid${pronoun} a Auxie!`,
+        })
+
         res.status(200).json('Servicio solicitado con éxito')
     } catch (error) {
         res.status(400).json({ error: error.message })
