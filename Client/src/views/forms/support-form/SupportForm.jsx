2b776fadd2fb208d67435813a2a7da9d17fffaff
franjunoy <franjunoy@hotmail.com>
2023-08-11 02:41:58 -0300

    Arrelge un poco el formulario de reclamo

--- Client/src/views/forms/support-form/SupportForm.jsx
+++ Client/src/views/forms/support-form/SupportForm.jsx
@@ -1,7 +1,7 @@
 import style from './SupportForm.module.scss'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 // import { useValidations } from '../../../utils/validationutils'
 import axios from 'axios'
 
@@ -22,16 +22,19 @@ const validate = (input) => {
     return errors
 }
 const SupportForm = () => {
-    const token = useSelector(state => state.token)
+    const token = useSelector((state) => state.token)
     const [errors, setErrors] = useState({})
     const [isFormValid, setIsFormValid] = useState(false)
+    const [isSubmitted, setIsSubmitted] = useState(false)
     const [input, setInput] = useState({
         consumerUsername: '',
         message: '',
         providerUsername: '',
         reason: '',
-        image: ''
+        image: '',
     })
+
+    const [selectedImage, setSelectedImage] = useState(null)
 
     const checkFormValidity = () => {
         const formIsValid = Object.keys(errors).length === 0
@@ -50,34 +53,57 @@ const SupportForm = () => {
         })
         setErrors(updatedErrors)
         checkFormValidity()
-    } 
+    }
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0]
+
+        if (file) {
+            setSelectedImage(file)
+            setInput((prevInput) => ({
+                ...prevInput,
+                image: file.name,
+            }))
+
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                image: '',
+            }))
+        }
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        
         if (Object.values(errors).length > 0) {
             alert('Por favor completa todos los campos')
         } else {
             try {
-                const response = await axios.post(
-                    '/claims/',
-                    input, {
-                        headers: {
-                            'authorization': `Bearer ${token}`
-                        }
-                    }
-                )
+                const formData = new FormData()
+                formData.append('image', selectedImage)
+                formData.append('consumerUsername', input.consumerUsername)
+                formData.append('providerUsername', input.providerUsername)
+                formData.append('message', input.message)
+                formData.append('reason', input.reason)
+
+                const response = await axios.post('/claims/', formData, {
+                    headers: {
+                        authorization: `Bearer ${token}`,
+                        'Content-Type': 'multipart/form-data',
+                    },
+                })
                 setInput({
                     consumerUsername: '',
                     providerUsername: '',
                     message: '',
                     image: '',
-                    reason: ''
+                    reason: '',
                 })
+                setSelectedImage(null)
+
                 if (response) {
-                    alert('Solicitud enviada')
-                    console.log(response)
+                    setIsSubmitted(true)
                 }
-                setIsFormValid(false)
             } catch (error) {
                 alert('Error al enviar la solicitud:' + error.message)
             }
@@ -87,111 +113,132 @@ const SupportForm = () => {
     return (
         <div>
             <div className={style.claims}>
-                <h1>Soporte técnico</h1>
-                <form id="form" onSubmit={handleSubmit}>
+                {isSubmitted ? (
                     <div>
-                        <label
-                            htmlFor="consumerUsername"
-                            className={style.formlabel}
-                        >
-                            Nombre de Usuario:
-                        </label>
-                        <input
-                            type="text"
-                            name="consumerUsername"
-                    
-                            className={style.formControl}
-                            value={input.consumerUsername}
-                            onChange={(e) => handleChange(e)}
-                            placeholder="Nombre de usuario"
-                        />
-                        {errors.consumerUsername && (
-                            <p className={style.error}>{errors.consumerUsername}</p>
-                        )}
+                        <p>¡Tu reclamo ha sido enviado con éxito!</p>
+                        <Link to="/">Volver a la página principal</Link>
                     </div>
+                ) : (
                     <div>
-                        <label htmlFor="message" className={style.formlabel}>
-                            Reclamo
-                        </label>
-                        <textarea
-                            name="message"
-                            value={input.message}
-                            onChange={(e) => handleChange(e)}
-                            placeholder="Escribe tu reclamo aquí"
-                        />
-                        {errors.message && (
-                            <p className={style.error}>{errors.message}</p>
-                        )}
+                        <h1>Soporte técnico</h1>
+                        <form id="form" onSubmit={handleSubmit}>
+                            <div>
+                                <label
+                                    htmlFor="consumerUsername"
+                                    className={style.formlabel}
+                                >
+                                    Nombre de Usuario:
+                                </label>
+                                <input
+                                    type="text"
+                                    name="consumerUsername"
+                                    className={style.formControl}
+                                    value={input.consumerUsername}
+                                    onChange={(e) => handleChange(e)}
+                                    placeholder="Nombre de usuario"
+                                />
+                                {errors.consumerUsername && (
+                                    <p className={style.error}>
+                                        {errors.consumerUsername}
+                                    </p>
+                                )}
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="message"
+                                    className={style.formlabel}
+                                >
+                                    Reclamo
+                                </label>
+                                <textarea
+                                    name="message"
+                                    value={input.message}
+                                    onChange={(e) => handleChange(e)}
+                                    placeholder="Escribe tu reclamo aquí"
+                                />
+                                {errors.message && (
+                                    <p className={style.error}>
+                                        {errors.message}
+                                    </p>
+                                )}
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="providerUsername"
+                                    className={style.formlabel}
+                                >
+                                    Nombre del Auxie
+                                </label>
+                                <input
+                                    type="text"
+                                    name="providerUsername"
+                                    value={input.providerUsername}
+                                    onChange={(e) => handleChange(e)}
+                                    placeholder="Nombre del Auxie"
+                                />
+                                {errors.providerUsername && (
+                                    <p className={style.error}>
+                                        {errors.providerUsername}
+                                    </p>
+                                )}
+                            </div>
+                            <div>
+                                <label>Imagen</label>
+                                <input
+                                    type="file"
+                                    accept=".jpg, .jpeg, .png"
+                                    name="image"
+                                    onChange={(e) => handleImageChange(e)}
+                                />
+                                {errors.image && <p>{errors.image}</p>}
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="reason"
+                                    className={style.formlabel}
+                                >
+                                    Selección de Motivos
+                                </label>
+                                <div>
+                                    <select
+                                        name="reason"
+                                        value={input.reason}
+                                        onChange={(e) => handleChange(e)}
+                                    >
+                                        <option disabled value="">
+                                            Motivos
+                                        </option>
+                                        <option value="late">Retraso</option>
+                                        <option value="behavior">
+                                            Comportamiento inadecuado
+                                        </option>
+                                        <option value="quality">
+                                            Calidad del servicio
+                                        </option>
+                                        <option value="other">Otro</option>
+                                    </select>
+                                    {errors.reason && (
+                                        <p className={style.error}>
+                                            {errors.reason}
+                                        </p>
+                                    )}
+                                </div>
+                            </div>
+                            <div>
+                                <button
+                                    type="submit"
+                                    className={style.claims}
+                                    disabled={!isFormValid}
+                                >
+                                    Enviar
+                                </button>
+                                <Link to="/">
+                                    <button>Atrás</button>
+                                </Link>
+                            </div>
+                        </form>
                     </div>
-                    <div>
-                        <label
-                            htmlFor="providerUsername"
-                            className={style.formlabel}
-                        >
-                            Nombre del Auxie
-                        </label>
-                        <input
-                            type="text"
-                            name="providerUsername"
-                            value={input.providerUsername}
-                            onChange={(e) => handleChange(e)}
-                            placeholder="Nombre del Auxie"
-                        />
-                        {errors.providerUsername && (
-                            <p className={style.error}>
-                                {errors.providerUsername}
-                            </p>
-                        )}
-                    </div>
-                    <div>
-                        <label>Imagen</label>
-                        <input
-                            type="text"
-                            value={input.image}
-                            name="image"
-                            onChange={(e) => handleChange(e)}
-                        />
-                        {errors.image && <p>{errors.image}</p>}
-                    </div>
-                    <div>
-                        <label htmlFor="reason" className={style.formlabel}>
-                            Selección de Motivos
-                        </label>
-                        <div>
-                            <select
-                                name="reason"
-                                value={input.reason}
-                                onChange={(e) => handleChange(e)}
-                               
-                            >
-                                <option disabled value="">
-                                    Motivos
-                                </option>
-                                <option value="late">Retraso</option>
-                                <option value="behavior">
-                                    Comportamiento inadecuado
-                                </option>
-                                <option value="quality">
-                                    Calidad del servicio
-                                </option>
-                                <option value="other">Otro</option>
-                            </select>
-                            {errors.reason && (
-                                <p className={style.error}>{errors.reason}</p>
-                            )}
-                        </div>
-                    </div>
-                    <button
-                        type="submit"
-                        className={style.claims}
-                        disabled={!isFormValid}
-                    >
-                        Enviar
-                    </button>
-                    <Link to="/">
-                        <button>Atrás</button>
-                    </Link>
-                </form>
+                )}
             </div>
         </div>
     )
