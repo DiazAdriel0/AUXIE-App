67fb9152448a32a0d6f531a37bd6dd54d464006d
franjunoy <franjunoy@hotmail.com>
2023-08-11 17:04:10 -0300

    Falta hacer algo con Adri por las fotos

--- Client/src/views/profile/profileAuxies/profileAuxies.jsx
+++ Client/src/views/profile/profileAuxies/profileAuxies.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { updateProfile } from '../../../redux/Actions/actions'
 
 const ProfileAuxies = () => {
-    const dispatch = useDispatch
     const provider = useSelector((state) => state.loggedUser)
+    const token = useSelector((state) => state.token)
     const [newImage, setNewImage] = useState('')
-    const [error, setError] = useState(null)
     const [newBio, setNewBio] = useState('')
+    const [error, setError] = useState(null)
+    const dispatch = useDispatch
 
-    const handleImageChange = (e) => {
-        const file = e.target.files[0]
+    const handleImageChange = (elem) => {
+        const file = elem.target.files[0]
         if (file && (file.type === 'image/jpeg' || file.type === 'image/png')) {
             setNewImage(file)
             setError(null)
@@ -23,9 +25,10 @@ const ProfileAuxies = () => {
     }
 
     const handleUpdateProfile = () => {
-        dispatch(
-            updateUserProfile(provider.id, { image: newImage, bio: newBio })
-        )
+        const formData = new FormData()
+        formData.append('image', newImage)
+        formData.append('bio', newBio)
+        dispatch( updateProfile({ image: newImage, bio: newBio }, token, 'consumers'))
     }
 
     return (
@@ -37,15 +40,13 @@ const ProfileAuxies = () => {
                     accept=".jpg, .png"
                     onChange={handleImageChange}
                 />
-                <h1>
-                    {provider.firstName} {provider.lastName}
-                </h1>
-                <h4>Gender: {provider.gender}</h4>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                <h1>{provider.firstName} {provider.lastName}</h1>
+                <h4>Genero: {provider.gender}</h4>
                 <h3>
-                    Email: {provider.email} <button>Cambiar contraseña</button>
+                    Email: {provider.email}{' '} <button>Cambiar contraseña</button>
                 </h3>
                 <textarea value={newBio} onChange={handleBioChange} />
-                <button onClick={handleUpdateProfile}>Guardar Cambios</button>
                 <h6>Te uniste: {provider.registerDate}</h6>
                 <div>
                     <h5>Servicios que ofrece:</h5>
@@ -54,6 +55,7 @@ const ProfileAuxies = () => {
                     <h5>Average Rating: {provider.averageRating}</h5>
                     <h5>Reviews:</h5>
                 </div>
+                <button onClick={handleUpdateProfile}>Guardar Cambios</button>
             </div>
         </div>
     )
