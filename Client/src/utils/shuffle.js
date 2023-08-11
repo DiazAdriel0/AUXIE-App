da621d2794eabb2c67a59c73806fdc823364d4b5
BonPato98 <bonfiglipatricio12@hotmail.com>
2023-08-11 14:06:12 -0300

    feat(client(landing) featured auxies

---
+++ Client/src/utils/shuffle.js
@@ -0,0 +1,13 @@
+ // shuffle function
+ export function shuffle(array) {
+   let currentIndex = array.length,  randomIndex;
+
+   while (currentIndex != 0) {
+     randomIndex = Math.floor(Math.random() * currentIndex);
+     currentIndex--;
+
+     [array[currentIndex], array[randomIndex]] = [
+       array[randomIndex], array[currentIndex]];
+   }
+   return array;
+ }
