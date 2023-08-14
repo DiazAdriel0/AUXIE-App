30fba8f8ecba7ce58b4167ecb02707fe27324cf3
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-14 07:00:36 -0300

    fixed errors of rendering

--- Client/src/components/clientRequiredService/ClientRequiredService.jsx
+++ Client/src/components/clientRequiredService/ClientRequiredService.jsx
@@ -1,21 +1,36 @@
 import ButtonMercadoPago from '../buttonMercadoPago/ButtonMercadoPago'
 
 const ClientRequiredService = (job) => {
-   const {id, service, requestDate, description, status, jobDate, price, paymentMethod} = job
-   const mercadoPago = paymentMethod === 'app' ? true : false
-  //  const approved = status === 'approved' ? true : false
-  return (
-    <div>
-        {id}
-        {requestDate}
-        {description}
-        {service}
-        {jobDate}
-        {price}
-        {status}
-        {mercadoPago ? (<ButtonMercadoPago price={price} description={service} quantity={1}/>) : null}
-    </div>
-  )
+    const {
+        id,
+        service,
+        requestDate,
+        description,
+        status,
+        jobDate,
+        price,
+        paymentMethod,
+    } = job
+    const mercadoPago = paymentMethod === 'app' ? true : false
+    //  const approved = status === 'approved' ? true : false
+    return (
+        <div>
+            {id}
+            {requestDate}
+            {description}
+            {service}
+            {jobDate}
+            {price}
+            {status}
+            {mercadoPago ? (
+                <ButtonMercadoPago
+                    price={price}
+                    description={service}
+                    quantity={1}
+                />
+            ) : null}
+        </div>
+    )
 }
 
 export default ClientRequiredService
