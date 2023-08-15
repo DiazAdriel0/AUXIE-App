c3e1d6e460b02ed26012844732404a713e083f12
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-15 15:00:50 -0300

    Connection with socket.io

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const server = require('./App/app')
+const { Server } = require('socket.io')
+const { createServer } = require('http')
 
 const { MONGO_DB_CONNECTION, PORT } = process.env
+
+const httpServer = createServer(server)
 
 const port = PORT || 3001
 
@@ -12,10 +16,30 @@ mongoose
     .connect(connectionString)
     .then(() => {
         console.log('Database Connected')
-        server.listen(port, () => {
+        httpServer.listen(port, () => {
             console.log(`server raised on port: ${port}`)
         })
     })
     .catch((error) => {
         console.error(error)
     })
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: ['http://localhost:5173', 'https://auxie-app.vercel.app'],
+    },
+})
+
+io.on('connection', (socket) => {
+    console.log('User connected')
+
+    socket.on('chatMessage', (message) => {
+        // Lógica para manejar el mensaje recibido desde el cliente
+        // Y luego puedes emitir eventos para enviar mensajes a todos los clientes conectados
+        io.emit('chatMessage', message)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected')
+    })
+})
