30dcb99490de88b4aa8510033b9b8acd00f9af58
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-15 00:26:51 -0300

    Form to change price created

---
+++ Client/src/views/forms/price-form/PriceForm.jsx
@@ -0,0 +1,11 @@
+import style from './priceForm.module.scss'
+
+const PriceForm = () => {
+    return (
+        <div>
+            <input type="text"></input>
+        </div>
+    )
+}
+
+export default PriceForm
