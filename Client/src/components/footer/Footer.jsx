8625c6cc5bc8f216b045c3829b3823e77cc62421
AgustinaFernandez01 <agustinafer29@gmail.com>
2023-08-15 22:45:03 -0300

    footer

--- Client/src/components/footer/Footer.jsx
+++ Client/src/components/footer/Footer.jsx
@@ -1,25 +1,52 @@
-import style from './footer.module.scss' 
+import style from './footer.module.scss'
 import CircleIconAuxie from '../../assets/logos/CircleIconAuxie.png'
-import {Link}  from 'react-router-dom'
+import pinLocation from '../../assets/icons/pin-location.svg'
+import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
         <footer className={style.landingFooter}>
-                        <div className={style.divFooterTitle}>
-                            <img
-                                src={CircleIconAuxie}
-                                alt="circle icon"
-                                className={style.divFooterImg}
-                            />
-                            <div className={style.links}>
-                            <p><Link>Condiciones de Uso</Link></p>
-                            <p><Link>Políticas de Privacidad</Link></p>
-                            <p><Link>Condiciones de Uso</Link></p>    
-                            </div>
-                        </div>
-                        <div className={style.divCopy}>
-                            <p>Auxie © 2023 Auxie Team. Todos los derechos reservados.</p>
-                        </div>
-                    </footer>
+            <img
+                src={CircleIconAuxie}
+                alt="circle icon"
+                className={style.divFooterImg}
+            /> 
+            <div className={style.section}>
+
+            <div className={style.divFooterTitle}>
+                <p>Descubri</p>
+                <p >
+                    <Link>Convertite en Auxie</Link>
+                </p>
+                <p >
+                    <Link>Servicios en tu ciudad</Link>
+                </p>
+                <p >
+                    <Link>Todos los servicios</Link>
+                </p>
+                <p >
+                    <Link>Ayuda</Link>
+                </p> 
+                </div>             
+                <div className={style.social}>
+                    <p>Español(Internacional)</p>
+                    <p><img src={pinLocation}></img>Argentina y Mexico</p>
+                </div>   
+            </div>
+                <div className={style.links}>
+                    <p>
+                        <Link to='/aboutUs'>Auxie Team</Link>
+                    </p>
+                    <p>
+                        <Link>Políticas de Privacidad</Link>
+                    </p>
+                    <p>
+                        <Link>Condiciones de Uso</Link>
+                    </p>
+            </div>
+            <div className={style.divCopy}>
+                <p>Auxie © 2023 Auxie Team. Todos los derechos reservados.</p>
+            </div>
+        </footer>
     )
 }
 
