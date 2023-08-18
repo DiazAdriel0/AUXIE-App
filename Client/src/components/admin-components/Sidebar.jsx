3e81421569e9c2ff7db237c18e9a5a8956c1c7cc
Joshua Candia <joshuacandia74@gmail.com>
2023-08-18 01:13:13 -0300

    update(Client/Sidebar):final update of sidebar component

--- Client/src/components/admin-components/Sidebar.jsx
+++ Client/src/components/admin-components/Sidebar.jsx
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom'
 import { FcBullish } from 'react-icons/fc'
-import { DASHBOARD_SIDEBAR_LINKS } from '../../lib/consts/navigation'
+import { HiOutlineLogout } from 'react-icons/hi'
+import {
+    DASHBOARD_SIDEBAR_LINKS,
+    DASHBOARD_SIDEBAR_BOTTOM_LINKS,
+} from '../../lib/consts/navigation'
+import {
+    linkClassesDark,
+    linkClassesLight,
+    darkMode,
+    lightMode,
+} from '../../lib/consts/styles'
 import { useSelector } from 'react-redux'
 import ButtonLightNight from '../buttons/button-light-night/ButtonLightNight'
-
+import classNames from 'classnames'
 import { useLocation } from 'react-router-dom'
 
 const Sidebar = () => {
     const nightMode = useSelector((state) => state.nightMode)
-    const darkMode = 'bg-neutral-900 text-white w-60 p-3 flex flex-col h-screen'
-    const lightMode = 'bg-white text-black w-60 p-3 flex flex-col h-screen'
+
     return (
         <div className={nightMode ? darkMode : lightMode}>
             <div className='flex items-center gap-2 px-1 py-3'>
@@ -28,9 +37,32 @@ const Sidebar = () => {
                 {DASHBOARD_SIDEBAR_LINKS.map((item) => {
                     return <SideBarLink key={item.key} item={item} />
                 })}
+                <div className='pt-2 flex'>
+                    <ButtonLightNight />
+                </div>
             </div>
-            <div>
-                <ButtonLightNight />
+            <div className='flex flex-col gap-0.5 pt-2 border-t border-neutral-900'>
+                {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item) => {
+                    return <SideBarLink key={item.key} item={item} />
+                })}
+                <div
+                    className={
+                        nightMode
+                            ? classNames(
+                                  'text-red-400 cursor-pointer',
+                                  linkClassesDark
+                              )
+                            : classNames(
+                                  linkClassesLight,
+                                  'text-red-600 cursor-pointer'
+                              )
+                    }
+                >
+                    <span className='text-xl'>
+                        <HiOutlineLogout />
+                    </span>
+                    Logout
+                </div>
             </div>
         </div>
     )
@@ -39,10 +71,7 @@ const Sidebar = () => {
 function SideBarLink({ item }) {
     const { pathname } = useLocation()
     const nightMode = useSelector((state) => state.nightMode)
-    const linkClassesDark =
-        ' flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active-neutral-600 rounded-sm text-base'
-    const linkClassesLight =
-        ' flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-100 hover:no-underline active-neutral-600 rounded-sm text-base'
+
     return (
         <Link
             className={
