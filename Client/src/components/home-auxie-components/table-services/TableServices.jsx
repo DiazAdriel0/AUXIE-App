ceba98cf84a890a53dbbea8b2dd0022b429a4c63
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-17 23:32:37 -0300

    request model modified when the status changes

--- Client/src/components/home-auxie-components/table-services/TableServices.jsx
+++ Client/src/components/home-auxie-components/table-services/TableServices.jsx
@@ -70,7 +70,7 @@ const TableServices = () => {
             consumerId: service.clientId,
         }
 
-        /* dispatch(setServiceStatus(data)) */ //descomentar cuando se agregue lo de consumer a AddJobs
+        dispatch(setServiceStatus(data))
     }
 
     return (
--- Server/Controllers/ProvidersControllers/modifyJobStatus.js
+++ Server/Controllers/ProvidersControllers/modifyJobStatus.js
@@ -1,45 +1,54 @@
 const Provider = require('./../../Models/provider')
 const Consumer = require('./../../Models/consumer')
+const Request = require('./../../Models/request')
 
-const modifyJobStatus = async (req)=>{
-
-    const {id, consumerId, providerId, status} = req.body
+const modifyJobStatus = async (req) => {
+    const { id, consumerId, providerId, status } = req.body
     try {
-        
-        const consumer = await Consumer.findById({_id:consumerId}) //eslint-disable-line
-        const provider = await Provider.findById({_id:providerId})
-        const newJobs = provider.jobs.map((job)=>{
-            if(job.id === id){
+        const request = await Request.findById(id)
+        const consumer = await Consumer.findById({ _id: consumerId })
+        const provider = await Provider.findById({ _id: providerId })
+
+        request.status = status
+
+        await request.save()
+
+        const newJobs = provider.jobs.map((job) => {
+            if (job.id === id) {
                 let theJob = {
                     ...job,
                     status: status,
                 }
                 return theJob
-            }else{
+            } else {
                 return job
             }
         })
-        const newReqServices = consumer.requiredServices.map((service)=>{
-            if(service.providerId === providerId){ //xq la forma en la q estan 
-                let theService = {                  //los id del job es dudosa
+        const newReqServices = consumer.requiredServices.map((service) => {
+            if (service.id === id) {
+                let theService = {
                     ...service,
                     status: status,
                 }
                 return theService
-            }else{
+            } else {
                 return service
             }
         })
-        const update = await Provider.updateOne({_id:providerId},{jobs: newJobs})
-        const update2 = await Consumer.updateOne({_id:consumerId},{requiredServices: newReqServices})
-        if(!update.modifiedCount || !update2.modifiedCount)return false
+        const update = await Provider.updateOne(
+            { _id: providerId },
+            { jobs: newJobs }
+        )
+        const update2 = await Consumer.updateOne(
+            { _id: consumerId },
+            { requiredServices: newReqServices }
+        )
+        if (!update.modifiedCount || !update2.modifiedCount) return false
 
         return newJobs
- 
     } catch (error) {
         return false
     }
-    
 }
 
 module.exports = modifyJobStatus
