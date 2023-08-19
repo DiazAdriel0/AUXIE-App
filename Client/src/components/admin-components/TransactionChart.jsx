93257f73c5d709d7533fa795acac0bd0f3b0e982
Joshua Candia <joshuacandia74@gmail.com>
2023-08-19 17:25:38 -0300

    data seteado

--- Client/src/components/admin-components/TransactionChart.jsx
+++ Client/src/components/admin-components/TransactionChart.jsx
@@ -1,65 +1,4 @@
-const data = [
-    {
-        name: 'Ene',
-        expense: 2500,
-        income: 5500,
-    },
-    {
-        name: 'Feb',
-        expense: 2700,
-        income: 5200,
-    },
-    {
-        name: 'Mar',
-        expense: 2800,
-        income: 5100,
-    },
-    {
-        name: 'MaAbrr',
-        expense: 2600,
-        income: 5300,
-    },
-    {
-        name: 'May',
-        expense: 2900,
-        income: 5000,
-    },
-    {
-        name: 'Jun',
-        expense: 2500,
-        income: 5500,
-    },
-    {
-        name: 'Jul',
-        expense: 2700,
-        income: 5200,
-    },
-    {
-        name: 'Ago',
-        expense: 3000,
-        income: 4900,
-    },
-    {
-        name: 'Sep',
-        expense: 2400,
-        income: 5700,
-    },
-    {
-        name: 'Oct',
-        expense: 2800,
-        income: 5100,
-    },
-    {
-        name: 'Nov',
-        expense: 2600,
-        income: 5300,
-    },
-    {
-        name: 'Dec',
-        expense: 2900,
-        income: 5000,
-    },
-]
+import data from '../../lib/data/rechartData'
 
 import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 const TransactionChart = () => {
@@ -67,14 +6,14 @@ const TransactionChart = () => {
         <div className='h-[22rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1'>
             <strong className='text-gray-700 font-medium'>Transacciones</strong>
             <div className='w-full mt-3 flex-1 text-xs'>
-                <ResponsiveContainer width='100%' height='100%'>
+                <ResponsiveContainer width='100%' height='100%' aspect={6}>
                     <BarChart
                         width={500}
                         height={300}
                         data={data}
                         margin={{ top: 20, right: 10, left: -10, bottom: 0 }}
                     >
-                        <CartesianGrid strokeDasharray='3 3 0 0' vertical={false} />
+                        <CartesianGrid strokeDasharray='4 1 2' />
                         <XAxis dataKey='name' />
                         <YAxis />
                         <Tooltip />
