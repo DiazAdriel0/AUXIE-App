8b2c6d78fe9d2c6e70315528071bfdd87b998d26
DiazAdriel0 <diazadriel0@gmail.com>
2023-08-24 00:24:31 -0300

    firebase config

--- Server/config/firebase-config.js
+++ Server/config/firebase-config.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin')
 
+const serviceAccount = require('./serviceAccount.json')
+
 admin.initializeApp({
-    credential: admin.credential.cert(
-        JSON.parse(process.env.SERVICE_ACCOUNT_KEY)
-    ),
+    credential: admin.credential.cert(serviceAccount),
 })
 
 module.exports = admin
